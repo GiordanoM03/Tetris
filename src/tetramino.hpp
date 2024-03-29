2e89d41b692d072094ed441beb922cb93769dcaf
Mael Giordano <maelfrancois.giordano@gmail.com>
2024-03-29 12:51:28 +0100

    Creazione tetramino.hpp e tetramino.cpp

---
+++ src/tetramino.hpp
@@ -0,0 +1,17 @@
+#ifndef TETRAMINO_HPP
+#define TETRAMINO_HPP
+
+#include <cstring>
+
+class tetramino {
+    protected:
+        char name[4];
+        int RColor;
+        int GColor;
+        int Bcolor;
+        char* path = "../Tetramini";
+    public:
+
+};
+
+#endif
