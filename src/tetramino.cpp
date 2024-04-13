a33c396abb0f2dfad2e616f83ae19ab53bdfe884
IvanDV3 <divitaivan@gmail.com>
2024-04-13 12:16:54 +0200

    tetramini
    tetramini

--- src/tetramino.cpp
+++ src/tetramino.cpp
@@ -1,1 +1,57 @@
 #include "tetramino.hpp"
+#include <cstdlib>
+#include <iostream>
+
+const int Tetromino::I[4][4] = {
+    {0, 0, 0, 0},
+    {1, 1, 1, 1},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+};
+
+const int Tetromino::J[4][4] = {
+    {1, 0, 0, 0},
+    {1, 1, 1, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+};
+
+const int Tetromino::L[4][4] = {
+    {0, 0, 1, 0},
+    {1, 1, 1, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+};
+
+const int Tetromino::O[4][4] = {
+    {0, 0, 0, 0},
+    {0, 1, 1, 0},
+    {0, 1, 1, 0},
+    {0, 0, 0, 0}
+};
+
+const int Tetromino::S[4][4] = {
+    {0, 1, 1, 0},
+    {1, 1, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+};
+
+const int Tetromino::T[4][4] = {
+    {0, 1, 0, 0},
+    {1, 1, 1, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+};
+
+const int Tetromino::Z[4][4] = {
+    {1, 1, 0, 0},
+    {0, 1, 1, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+};
+
+int Tetromino::random() {
+    int N = rand() % 4;
+    return N;
+}
--- src/tetramino.hpp
+++ src/tetramino.hpp
@@ -1,16 +1,31 @@
-#ifndef TETRAMINO_HPP
-#define TETRAMINO_HPP
+#ifndef TETROMINO_HPP
+#define TETROMINO_HPP
 
-#include <cstring>
+class Tetromino {
+private:
+    static const int I[4][4];
+    static const int J[4][4];
+    static const int L[4][4];
+    static const int O[4][4];
+    static const int S[4][4];
+    static const int T[4][4];
+    static const int Z[4][4];
+public:   
 
-class tetramino {
-    protected:
-        char name[4];
-        int RColor;
-        int GColor;
-        int Bcolor;
-        char* path = "../Tetramini";
-    public:
+    enum TetrominoID {
+    I = 1,
+    J = 2,
+    L = 3,
+    O = 4,
+    S = 5,
+    T = 6,
+    Z = 7,
+};
+
+    int random();   //genera un numero da 0 a 3 per determinare con che rotazione buttare il tetramino iniziale
+
+    
+
 
 };
 
